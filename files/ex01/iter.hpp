15dce761f8e8fe3e0fec6e7cd461ce6430b65a42
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-04-03 16:09:23 +0200

    Add ex01

---
+++ files/ex01/iter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 15:06:04 by acrespy           #+#    #+#             */
+/*   Updated: 2024/04/03 15:10:27 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template <typename A, typename B, typename C>
+void iter(A *array, B length, C func)
+{
+	for (B i = 0; i < length; i++)
+		func(array[i]);
+}
+
+#endif
---
+++ files/ex01/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 15:02:20 by acrespy           #+#    #+#             */
+/*   Updated: 2024/04/03 15:02:21 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+#include <iostream>
+
+template <typename T>
+void printElement(T& element)
+{
+    std::cout << element << " ";
+}
+
+int main(void)
+{
+    int intArray[] = {1, 2, 3, 4, 5};
+    std::cout << "iter on int array:\n";
+    iter(intArray, 5, printElement<int>);
+    std::cout << std::endl;
+
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::cout << "iter on double array:\n";
+    iter(doubleArray, 5, printElement<double>);
+    std::cout << std::endl;
+
+    char charArray[] = {'a', 'b', 'c', 'd', 'e'};
+    std::cout << "iter on char array:\n";
+    iter(charArray, 5, printElement<char>);
+    std::cout << std::endl;
+
+    return (0);
+}
